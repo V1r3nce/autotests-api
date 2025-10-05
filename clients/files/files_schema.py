ce9a22bca95eac614dee8ba48dfb3e4cbf98d7c0
V1r3nce <seluanodenis@gmail.com>
2025-10-05 19:27:31 +0300

    Маркировки Pytest

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, HttpUrl
+from tools.fakers import fake
 
 
 class FileSchema(BaseModel):
@@ -10,12 +11,15 @@ class FileSchema(BaseModel):
     filename: str
     directory: str
 
+
 class CreateFileRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание файла.
     """
-    filename: str
-    directory: str
+    # Добавили генерацию случайного названия файла с расширением PNG
+    filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
+    # Директорию оставляем статичной, чтобы все тестовые файлы на сервере попадали в одну папку
+    directory: str = Field(default="tests")
     upload_file: str
 
 
